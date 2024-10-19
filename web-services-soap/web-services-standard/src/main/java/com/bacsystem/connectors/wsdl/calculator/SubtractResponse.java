f631f9d3f690e2e3cafd36b11df90c07ed42dd66
dbacilio88 <dbacilio88@outlook.es>
2024-10-18 20:06:37 -0500

    feat(github): action

--- web-services-soap/web-services-standard/src/main/java/com/bacsystem/connectors/wsdl/calculator/SubtractResponse.java
+++ web-services-soap/web-services-standard/src/main/java/com/bacsystem/connectors/wsdl/calculator/SubtractResponse.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.7 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.10.18 a las 04:02:58 PM COT 
+// Generado el: 2024.10.18 a las 08:02:04 PM COT 
 //
 
 
