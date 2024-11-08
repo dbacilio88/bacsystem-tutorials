e90e14571e14fd40bd3cacac767c25f5268a6ade
dbacilio88 <dbacilio88@outlook.es>
2024-11-08 15:21:45 -0500

    fix(github-action): version

--- web-services-soap/web-services-standard/src/main/java/com/bacsystem/connectors/wsdl/calculator/AddResponse.java
+++ web-services-soap/web-services-standard/src/main/java/com/bacsystem/connectors/wsdl/calculator/AddResponse.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.7 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.10.19 a las 12:24:01 AM PET 
+// Generado el: 2024.11.08 a las 03:21:27 PM PET 
 //
 
 
